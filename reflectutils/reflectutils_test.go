9fcb0393ca1fe649b7b344788e87e0a9cda33c37
Khiem Doan <doankhiem.crazy@gmail.com>
2022-11-07 15:45:38 +0700

    Rename files

---
+++ reflectutils/reflectutils_test.go
@@ -0,0 +1,28 @@
+package reflectutils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewObject(t *testing.T) {
+	type abc struct {
+		number int
+		text   string
+	}
+
+	x := abc{
+		number: 12,
+		text:   "abcxyz",
+	}
+
+	y := NewObject(x)
+
+	if reflect.TypeOf(y) != reflect.TypeOf(x) {
+		t.Error("NewObject was incorrect")
+	}
+
+	if _, ok := y.(abc); !ok {
+		t.Error("NewObject was incorrect")
+	}
+}
